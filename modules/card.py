6879047e13818d077c5a4b81717fc97b2e766701
DeVitoF <filippodevito2007@gmail.com>
2025-01-08 08:40:10 +0000

    Finale 1

--- modules/card.py
+++ modules/card.py
@@ -3,16 +3,16 @@ class Card:
         self.rank = rank
         self.suit = suit
 
-        if suit == "Denari":
-            short_suit = "D"
-        elif suit == "Coppe":
-            short_suit = "C"
-        elif suit == "Spade":
-            short_suit = "S"
-        elif suit == "Bastoni":
-            short_suit = "B"
+        if self.suit == "Denari":
+            self.short_suit = "D"
+        elif self.suit == "Coppe":
+            self.short_suit = "C"
+        elif self.suit == "Spade":
+            self.short_suit = "S"
+        elif self.suit == "Bastoni":
+            self.short_suit = "B"
 
-        self.image_location = 'static/images/{}{}.png'.format(
+        self.image_location = "static/images/{}{}.jpg".format(
             self.rank, self.short_suit)
 
     @property
