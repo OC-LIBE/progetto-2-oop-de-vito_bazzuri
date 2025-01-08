eabe4f9a63cdb37112dc53aeb03c5df4877d8e04
DeVitoF <filippodevito2007@gmail.com>
2025-01-08 08:25:47 +0000

    creazione branch

--- modules/card.py
+++ modules/card.py
@@ -2,35 +2,18 @@ class Card:
     def __init__(self, rank, suit):
         self.rank = rank
         self.suit = suit
-        if self.rank == 1:
-            self.card_scores = [1, 11]
-        elif self.rank >= 11 and self.rank <= 13:
-            self.card_scores = [10, 10]
-        else:
-            self.card_scores = [self.rank, self.rank]
 
-        if self.rank == 1:
-            self.short_rank = 'A'
-        elif self.rank == 11:
-            self.short_rank = 'J'
-        elif self.rank == 12:
-            self.short_rank = 'Q'
-        elif self.rank == 13:
-            self.short_rank = 'K'
-        else:
-            self.short_rank = str(self.rank)
-
-        if self.suit == 'Spades':
-            self.short_suit = 'S'
-        elif self.suit == 'Hearts':
-            self.short_suit = 'H'
-        elif self.suit == 'Clubs':
-            self.short_suit = 'C'
-        else:
-            self.short_suit = 'D'
+        if suit == "Denari":
+            short_suit = "D"
+        elif suit == "Coppe":
+            short_suit = "C"
+        elif suit == "Spade":
+            short_suit = "S"
+        elif suit == "Bastoni":
+            short_suit = "B"
 
         self.image_location = 'static/images/{}{}.png'.format(
-            self.short_rank, self.short_suit)
+            self.rank, self.short_suit)
 
     @property
     def image(self):
