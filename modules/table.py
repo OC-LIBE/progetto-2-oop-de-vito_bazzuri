925bcd234a754eafd82c64e58351adbd4a119de3
bazzuril <bazzuriloris@gmail.com>
2025-01-29 08:45:20 +0000

    veramente commit precedente

---
+++ modules/table.py
@@ -0,0 +1,30 @@
+from modules.card import Card
+from modules.deck import Deck
+from modules.player import Player
+
+class Table:
+    def __init__ (self, briscola, your_cards, my_cards; win_hand, opponent, me ):
+        self.briscola = briscola
+        self.your_cards = your_cards
+        self.my_cards = my_cards
+        self.win_hand = win_hand
+        self.me = me 
+        self.opponent = opponent
+   
+    def power(self):
+        if self.my_cards == self.briscola and self.your_cards != self.briscola:
+            self.me 
+        elif self.your_cards == self.briscola and self.my_cards != self.briscola:
+        for self.my_cards in self.short.suit:
+            
+
+
+
+
+    
+
+
+
+
+       
+if 'table' not in st.session_state:
