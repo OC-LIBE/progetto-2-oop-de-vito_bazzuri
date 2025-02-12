c6ff1c6907d26d828e5b52dfcac8996e2f7a03d0
DeVitoF <filippodevito2007@gmail.com>
2025-02-12 08:09:31 +0000

    Si può giocare la carta

--- app.py
+++ app.py
@@ -4,52 +4,48 @@ from modules.player import Player
 
 # SETTING UP THE GAME
 if 'game' not in st.session_state:
+    st.session_state['grafica'] = -1
     st.session_state['game'] = Game()
 
 st.set_page_config(layout="wide")
 game = st.session_state['game']
 
-# MANO AVVERSARIO
-col1, col2, col3, col4,col5, col0 = st.columns([0.1,0.1,0.1,0.1,0.1,0.5]) # Col0 è vuota per lasciare spazio vuoto -> Grafica
-with col1:
-    st.image(game.bot.cards[0].image)
-with col3:
-    st.image(game.bot.cards[1].image)    
-with col5:
-    st.image(game.bot.cards[2].image)
-    
-
-# CENTRO TAVOLA
-col1, col2, col3 = st.columns([0.2,0.2,0.6])
-with col1: #MAZZO
-    st.image("static/images/RETRO.png", caption= game.deck.remaining)
-
-with col3: #BRISCOLA
-    st.image(game.deck.last.image)
-
-with col5: #TABLE
-    st.write("Nulla Ancora")
+def colonna(nr_col=0):
+    st.image(game.bot.cards[nr_col].image)
+    if nr_col == 0:
+        st.image("static/images/RETRO.png", caption= game.deck.remaining)
+    elif nr_col == 1:
+        st.image("static/images/VUOTO.png",width=89) # Width per motivi estetici
+    elif nr_col == 2:
+        st.image(game.deck.last.image, caption = "Briscola")
+    if st.session_state['grafica'] != nr_col and game.player.cards[nr_col] != None:
+        st.image(game.player.cards[nr_col].image)
+        st.write(game.player.cards[nr_col])
+        if st.session_state['grafica'] == -1:
+            if st.button("Gioca",key=nr_col):
+                st.session_state['grafica'] = nr_col
+                if game.table.first_card == None:
+                    game.table.first_card = game.player.cards[nr_col]
+                else:
+                    game.table.second_card = game.player.cards[nr_col]
+                game.player.cards[nr_col] = None
 
 # MANO TUA
 col1, col2, col3, col4, col5, col0 = st.columns([0.1,0.1,0.1,0.1,0.1,0.5]) # Col0 è vuota per lasciare spazio vuoto -> Grafica
+with col2:
+    if game.table.first_card != None:
+        st.image("static/images/VUOTO.png")
+        st.image(game.table.first_card.image)
+with col4:
+    if game.table.second_card != None:
+        st.image("static/images/VUOTO.png")
+        st.image(game.table.second_card.image)
 with col1:
-    st.image(game.player.cards[0].image)
-    st.write(game.player.cards[0])
-    if st.button("Gioca",key=0):
-        del(game.player.cards[0])
-        game.table.first_card = game.player.cards[0]
+    colonna(0)
 with col3:
-    st.image(game.player.cards[1].image)
-    st.write(game.player.cards[1])
-    if st.button("Gioca",key=1):
-        del(game.player.cards[1])
-        game.table.first_card = game.player.cards[1]
+    colonna(1)
 with col5:
-    st.image(game.player.cards[2].image)
-    st.write(game.player.cards[2])
-    if st.button("Gioca",key=3):
-        del(game.player.cards[2])
-        game.table.first_card = game.player.cards[2]
+    colonna(2)
 
 
 if st.button("Reload"):
